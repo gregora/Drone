bd8b14c47247c628f56edf84a8ecab51494505a4
Gregor A <gregor.alic@gimb.org>
2022-03-03 23:59:31 +0100

    Minor changes
    This reverts commit 12d18349d571b9bef8948738c0dd5d8cc4797c3d.

--- main.cpp
+++ main.cpp
@@ -37,9 +37,8 @@ int main(int argc, char* argv[]){
 	sf::Font font;
 	font.loadFromFile("/usr/share/fonts/truetype/tlwg/Garuda.ttf");
 	text.setFont(font);
-	text.setCharacterSize(20);
 	text.setFillColor(sf::Color(0, 0, 0));
-	text.setPosition(WIDTH - 90, 10);
+	text.setPosition(WIDTH - 100, 10);
 
 	sf::Text ltext;
 	ltext.setFont(font);
@@ -87,7 +86,7 @@ int main(int argc, char* argv[]){
 
 
 
-	int DRONE_NUMBER = 200;
+	int DRONE_NUMBER = 1000;
 	Drone drones[DRONE_NUMBER];
 	for(int i = 0; i < DRONE_NUMBER; i++){
 		window.clear();
